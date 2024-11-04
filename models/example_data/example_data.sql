4fe21280c348122ebeb66790af86691c1202d544
Russell Garner <russ@omni.co>
2024-11-04 19:16:22 +0000

    foo

--- models/example_data/example_data.sql
+++ models/example_data/example_data.sql
@@ -1,3 +1,3 @@
-{% for i in range( var("START") , var("END"), 2): %}
+{% for i in range( int(env_var("DBT_START",1)) , int(env_var("DBT_END",10)), 2): %}
     select {{ i }} as foo, {{ i + 1 }} as bar {% if not loop.last %} union all {% endif %}
 {% endfor %}
