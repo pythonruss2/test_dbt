4b277c1cb187253502f36ffbed85cba93a9f39a6
Scooter Magoo <russ@omni.co>
2026-08-04 20:52:08 +0000

    Push from Omni models/query_dbt.sql

--- models/query_dbt.sql
+++ models/query_dbt.sql
@@ -1,1 +1,8 @@
-select 1
+SELECT "BAR",
+    COALESCE(CASE
+        WHEN "BAR" = 0 THEN NULL
+        ELSE COALESCE("FOO", 0) / "BAR"
+    END, 0) * 5 AS "KEWL",
+    "FOO"
+FROM {{ref('example_data')}} AS "omni_dbt__example_data"
+GROUP BY 1, 3, 2
